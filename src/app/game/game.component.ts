bcd16374b82793f189476f4d2c6a7104a6db5e61
GandaMitsuo <panpeikun@web.de>
2023-10-20 11:06:25 +0200

    add game logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game();
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    console.log(this.game);
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCard.push(this.currentCard);
+        this.pickCardAnimation = false;
+        this.game.currentplayer++;
+        this.game.currentplayer = this.game.currentplayer % this.game.players.length;
+      }, 1000);
+
+    }
+  }
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((result: string) => {
+      if (result) {
+        this.game.players.push(result);
+      }
+    });
   }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,28 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCard: string[] = [];
+    public currentplayer: number = 0;
+
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+        shuffle(this.stack);
+    }
+}
+
+function shuffle(a) {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
